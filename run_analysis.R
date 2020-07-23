63439aab103d0408f5dc47998d409687c4a6666e
jajushubh <66201911+jajushubh@users.noreply.github.com>
2020-07-23 23:33:01 +0530

    Add files via upload

---
+++ run_analysis.R
@@ -0,0 +1,51 @@
+library(dplyr)
+##download the zip file on the course and unzipp it
+##after that
+features <- read.table("UCI HAR Dataset/features.txt", col.names = c("n","functions"))
+activities <- read.table("UCI HAR Dataset/activity_labels.txt", col.names = c("code", "activity"))
+subject_test <- read.table("UCI HAR Dataset/test/subject_test.txt", col.names = "subject")
+x_test <- read.table("UCI HAR Dataset/test/X_test.txt", col.names = features$functions)
+y_test <- read.table("UCI HAR Dataset/test/y_test.txt", col.names = "code")
+subject_train <- read.table("UCI HAR Dataset/train/subject_train.txt", col.names = "subject")
+x_train <- read.table("UCI HAR Dataset/train/X_train.txt", col.names = features$functions)
+y_train <- read.table("UCI HAR Dataset/train/y_train.txt", col.names = "code")
+
+##preparing the table
+x <- rbind(x_test,x_train)
+y <- rbind(y_test, y_train)
+subject <- rbind(subject_test,subject_train)
+Merge <- cbind(subject,y,x)
+
+## making the data tidy by keeping only mean and std columns
+
+##creating a logical vector for the columns required
+req_columns <- grepl("subject|code|mean|std",names(Merge),ignore.case = T)
+TidyData <- Merge[,req_columns]
+
+##changing the names of code to descriptive names
+TidyData$code <- activities[TidyData$code,2]
+
+##changing column names
+names(TidyData)[2] = "activity"
+names(TidyData)<-gsub("Acc", "Accelerometer", names(TidyData))
+names(TidyData)<-gsub("Gyro", "Gyroscope", names(TidyData))
+names(TidyData)<-gsub("BodyBody", "Body", names(TidyData))
+names(TidyData)<-gsub("Mag", "Magnitude", names(TidyData))
+names(TidyData)<-gsub("^t", "Time", names(TidyData))
+names(TidyData)<-gsub("^f", "Frequency", names(TidyData))
+names(TidyData)<-gsub("tBody", "TimeBody", names(TidyData))
+names(TidyData)<-gsub("-mean()", "Mean", names(TidyData), ignore.case = TRUE)
+names(TidyData)<-gsub("-std()", "STD", names(TidyData), ignore.case = TRUE)
+names(TidyData)<-gsub("-freq()", "Frequency", names(TidyData), ignore.case = TRUE)
+names(TidyData)<-gsub("angle", "Angle", names(TidyData))
+names(TidyData)<-gsub("gravity", "Gravity", names(TidyData))
+
+
+##finding the averages by each activity and subjecty
+
+FinalData <- TidyData %>%
+        group_by(subject,activity)%>%
+        summarise_all(funs(mean))
+write.table(FinalData,"FinalData.txt",row.name = F)
+
+
